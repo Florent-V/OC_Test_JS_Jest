ad34f9287c86112d031d05a69e0778c669834400
Florent-Vasseur <vasseurflorent@gmail.com>
2023-03-09 09:02:36 +0100

    test 01

---
+++ js/samples/unit1.test.js
@@ -0,0 +1,18 @@
+import { sayHello } from "./unit/unit1";
+
+describe('SayHello Unit Test Suites', () => {
+
+  it('should return "Hello World"', () => {
+    expect(sayHello()).toBe("Hello, World")
+  })
+
+  it('should return "Hello, Thomas"', () => {
+    expect(sayHello("Thomas")).toBe("Hello, Thomas")
+  })
+
+
+
+
+
+
+})
